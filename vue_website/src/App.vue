c3be34ede159d3686720242ead2eb4c1b8279ed3
Oliver <oliverschwab03@gmail.com>
2024-05-07 01:30:02 -0500

    Dynamicallt apply styles to all sections

--- vue_website/src/App.vue
+++ vue_website/src/App.vue
@@ -1,7 +1,33 @@
 <script setup lang="ts">
+import { onMounted, nextTick } from 'vue';
 import BioComponent from './components/BioComponent.vue'
 import EducationComponent from './components/EducationComponent.vue'
-import ContentTemplate from './components/ContentTemplate.vue'
+
+onMounted(async () => {
+  await nextTick(); // Wait for the DOM to update
+
+  const sections = document.getElementsByClassName('section');
+
+  Array.from(sections).forEach((section, i) => {
+    section.className = 'display__flex-v';
+    Array.from(section.children).forEach((element, j) => {
+      switch (j){
+        case 0:
+          element.className = 'display1 pad-bottom__s';
+          break;
+        case 1:
+          element.className = 'titlea pad-bottom__m';
+          break;
+        case 2:
+          element.className = 'titlec pad-bottom__m';
+          break;
+        default:
+          element.className = 'body';
+          break;
+      }
+    })
+  })
+});
 </script>
 
 <template>
@@ -19,5 +45,6 @@ import ContentTemplate from './components/ContentTemplate.vue'
   </main>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+
 </style>
--- vue_website/src/components/EducationComponent.vue
+++ vue_website/src/components/EducationComponent.vue
@@ -1,16 +1,20 @@
-<script setup>
-import ContentTemplate from './ContentTemplate.vue'
-</script>
-
 <template>
-    <ContentTemplate 
-        title="Education" 
-        subtitle='<a href = "https://www.sdsmt.edu/" class="rich-text">South Dakota Mines</a>' 
-        description=' B.S. Computer Science, Math Minor<br/>
-            Expected Graduation <span class="titlec__bold">December 2024</span> '
-        content="3.5 GPA<br/>
+    <div class="section" style="">
+        <h1 class="">
+            Education
+        </h1>
+        <a href = "https://www.sdsmt.edu/" class="rich-text">South Dakota Mines</a>
+        <p class="">
+            B.S. Computer Science, Math Minor<br/>
+            Expected Graduation <span class="titlec__bold">December 2024</span>
+        </p>
+        <p class=""> <!-- content -->
+            3.5 GPA<br/>
             Dean&apos;s List Spring 2023<br/>
-            Dean&apos;s List Fall 2023<br/><br/>" 
-    />
-    <!-- TODO: Add courses -->
+            Dean&apos;s List Fall 2023<br/><br/>
+        </p>
+        <div> 
+            <!-- TODO: Implemnt course list -->
+        </div>        
+    </div>
 </template>
